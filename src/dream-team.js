4fa4f04d6a9114de4b395f3a3a991e3305887029
ajiltOk <21summer12@gmail.com>
2023-04-14 22:45:57 +0300

    solved tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(Array.isArray(members) === true) {
+    let arr = [];
+  members.forEach(element => {
+    if(typeof(element) === 'string') {
+      let spaces = element.trim();
+      arr.push(spaces[0].toUpperCase());
+    }
+  })
+  return arr.sort().join('');
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(Array.isArray(arr) === true) {
+    let newArr2 = [];
+    let newArr = newArr2.concat(arr);
+    newArr.forEach(element => {
+      if(element === '--double-next') {
+        let ind = newArr.indexOf('--double-next');
+        if(ind < newArr.length - 1) {
+          newArr[ind] = newArr[ind + 1];
+        } else if(ind === newArr.length - 1) {
+          newArr.splice(ind, 1);
+        }
+      } else if(element === '--discard-prev') {
+        let ind = newArr.indexOf('--discard-prev');
+        if(ind != 0) {
+          newArr.splice((ind - 1), 2);
+        } else if(ind === 0) {
+          newArr.splice(ind, 1);
+        }
+      } else if(element === '--discard-next') {
+        let ind = newArr.indexOf('--discard-next');
+        if(newArr[ind + 2] === '--double-prev' || newArr[ind + 2] === '--discard-prev') {
+          newArr.splice(ind, 3);
+        } else {
+          if(ind < newArr.length - 1) {
+            newArr.splice((ind + 1), 2);
+          } else if(ind === newArr.length - 1) {
+            newArr.splice(ind, 1);
+          }
+        }
+      } else if(element === '--double-prev') {
+        let ind = newArr.indexOf('--double-prev');
+        if(ind != 0) {
+          newArr[ind] = newArr[ind - 1];
+        } else if(ind === 0) {
+          newArr.splice(ind, 1);
+        }
+      }
+    })
+    return newArr;
+  } else {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
 }
 
 module.exports = {
